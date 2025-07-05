66e79cf942f61022c71e9697d3016a432bf5fcd1
DPXJ <your-email@example.com>
2025-07-05 23:47:36 +0800

    添加 GitHub Actions 和 Pages 配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/test2025070501/', // 添加 base 配置，值为仓库名
   plugins: [react()],
   server: {
     host: '0.0.0.0',
